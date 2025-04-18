5a9f538cb00190fa00c3a0169016f7c8296b0607
PietroSM <pedrosalamora@gmail.com>
2025-04-18 18:43:49 +0200

    Commit_18_04_2025

--- routes/comanda.js
+++ routes/comanda.js
@@ -17,8 +17,6 @@ router.get('/', async(req, res) => {
     let resultat = validarToken(token);
     let idClient = resultat.id;
 
-
-
     try {
 
         const existeixComanda = await Comanda.find({ client: idClient })
@@ -112,7 +110,111 @@ router.get('/', async(req, res) => {
 });
 
 
+
+router.get('/vendes', async(req, res) => {
+    let token = req.headers['authorization'];
+    let resultat = validarToken(token);
+    let idClient = resultat.id;
+
+    try {
+        const existeixComanda = await Comanda.find({ client: idClient })
+        .populate({
+            path: 'productes.producte',
+            populate: {
+                path: 'client',
+                model: 'clients'
+            }
+        }).populate('client').populate('Vendedor')
+        .lean();
+
+        let comandes = [];
+            
+        if(existeixComanda) {
+            
+        existeixComanda.forEach(comanda => {
+                
+            let productes = [];
+
+            comanda.productes.forEach(element => {
+
+                productes.push({
+                    producte: {
+                        'nom': element.producte.nom,
+                        'stock': element.producte.stock,
+                        'preu': element.producte.preu,
+                        'imatge': element.producte.imatge,
+                        'lat': element.producte.lat,
+                        'lng': element.producte.lng,
+                        'enviament':element.producte.enviament,
+                        'temporada': element.producte.temporada,
+                        'tipus': element.producte.tipus,
+                        'id': element.producte._id,
+                        'client': {
+                            'id': element.producte.client._id,
+                            'nom': element.producte.client.nom,
+                            'preu': element.producte.client.preu,
+                            'imatge': element.producte.client.imatge,
+                            'lat': element.producte.client.lat,
+                            'lng': element.producte.client.lng,
+                            'enviament': element.producte.client.enviament,
+                            'recogida': element.producte.client.recogida,
+                            'temporada': element.producte.client.temporada,
+                            'tipus': element.producte.client.tipus
+                        }
+                    },
+                    quantitat: element.quantitat,
+                    preu: element.preu
+                });
+
+            });
+
+            comandes.push({
+                id: comanda._id,
+                client : {
+                    'id': comanda.client._id,
+                    'nom': comanda.client.nom,
+                    'cognom': comanda.client.cognom,
+                    'correu': comanda.client.correu,
+                    'imatge': comanda.client.imatge,
+                    'lat': comanda.client.lat,
+                    'lng': comanda.client.lng,
+                },
+                productes: productes,
+                preuTotal: comanda.preuTotal,
+                estatComanda: comanda.estatComanda,
+                enviament: comanda.enviament,
+                puntRecogida: comanda.puntRecogida,
+                Vendedor: {
+                    'id': comanda.Vendedor._id,
+                    'nom': comanda.Vendedor.nom,
+                    'cognom': comanda.Vendedor.cognom,
+                    'correu': comanda.Vendedor.correu,
+                    'imatge': comanda.Vendedor.imatge,
+                    'lat': comanda.Vendedor.lat,
+                    'lng': comanda.Vendedor.lng,
+                }
+            });
+
+
+        });
+
+        console.log(comandes);
+        res.status(200).send({comandes});
+    }
+        
+    } catch (error) {
+        
+    }
+
+
+});
+
+
 router.get('/:id', async(req, res) => {
+    let token = req.headers['authorization'];
+    let resultat = validarToken(token);
+    let idClient = resultat.id;
+
     try{
         const comanda = await Comanda.findById(req.params.id)
                 .populate({
@@ -185,7 +287,8 @@ router.get('/:id', async(req, res) => {
                     'imatge': comanda.Vendedor.imatge,
                     'lat': comanda.Vendedor.lat,
                     'lng': comanda.Vendedor.lng,
-                }
+                },
+                me: comanda.Vendedor._id == idClient ? true : false
             }});
 
 
